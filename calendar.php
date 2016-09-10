436b8b3fec44a0538122c74feb5bffbe5136b06d
Tigermisu <tigermisu@outlook.com>
2016-09-10 15:09:39 -0600

    better calendar

--- calendar.php
+++ calendar.php
@@ -17,6 +17,26 @@
                     <div class="month-name"></div>
                     <div class="controls">
                         <span class="glyphicon glyphicon-chevron-left"></span>
+                        <div class="dropdown">
+                            <button id="dropdownMonth" type="button" data-toggle="dropdown">
+                                Month
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>January</li>
+                                <li>February</li>
+                                <li>March</li>
+                                <li>April</li>
+                                <li>May</li>
+                                <li>June</li>
+                                <li>July</li>
+                                <li>August</li>
+                                <li>September</li>
+                                <li>October</li>
+                                <li>November</li>
+                                <li>December</li>
+                            </ul>
+                        </div>
                         <span class="glyphicon glyphicon-chevron-right"></span>
                     </div>
                 </div>
@@ -144,7 +164,45 @@
     </div>
 
     <div class="std-section">
-    
+        
+    </div>
+
+    <div class="modal fade" tabindex="-1" role="dialog" id="eventModal">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body">
+                    <div id="imageWrapper" class="modal-image-wrapper">
+                        <div id="mainImage" class="hidden"></div>
+                        <div id="sliderWrapper" class="hidden">
+                            <div id="modalSlider" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#modalSlider" data-slide-to="0" class="active"></li>
+                                    <li data-target="#modalSlider" data-slide-to="1"></li>
+                                    <li data-target="#modalSlider" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner" role="listbox">
+                                
+                                </div>
+                                <a class="left carousel-control" href="#modalSlider" role="button" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#modalSlider" role="button" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                                </div>
+                        </div>
+                    </div>
+                    <p class="modal-text"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php include('footer.html'); ?>
--- js/app.js
+++ js/app.js
@@ -139,6 +139,40 @@ calendar = {
         "July", "August", "September", "October", "November", "December"
         ],
 
+    categories: {
+        "Meeting": "http://igem.org/wiki/images/thumb/d/d7/Meeting.jpg/308px-Meeting.jpg.png",
+        "Collaboration": "http://igem.org/wiki/images/thumb/5/54/Collaboration.jpg/320px-Collaboration.jpg.png",
+        "Project": "http://igem.org/wiki/images/thumb/1/14/ProjectDN.jpg/155px-ProjectDN.jpg.png",
+        "Human Practices": "http://igem.org/wiki/images/thumb/c/ce/HP.jpg/282px-HP.jpg.png",
+        "Diffusion": "http://igem.org/wiki/images/thumb/a/ac/Diffusion.jpg/200px-Diffusion.jpg.png",
+        "iGEM": "http://2016.igem.org/wiki/images/3/30/T--Tec-Chihuahua--igem-small.png"
+
+    },
+
+    data: [
+        [
+            {
+                Título: "First official meeting of the team",
+                Categoría: "Meeting",
+                Mes: "01",
+                Día: "7",
+                Imagen_principal: ["http://igem.org/wiki/images/thumb/1/10/7enero.jpg/800px-7enero.jpg"],
+                Texto: "Today we had our first official meeting!<br>iGEM Tec Chihuahua is integrated by 14 undergraduate students from 3rd, 5th and 7th semester of Engineering Biotechnology, and our two advisors: Cd.Ph.D. Cynthia Lorena González Trevizo and Ph.D. Silvia Lorena Montes Fonseca."
+            },
+
+            {
+                Título: "Multiple images test",
+                Categoría: "Human Practices",
+                Mes: "01",
+                Día: "7",
+                Imagen_principal: ["http://igem.org/wiki/images/thumb/1/10/7enero.jpg/800px-7enero.jpg",
+                                   "http://lorempixel.com/800/720",
+                                   "http://lorempixel.com/800/800"],
+                Texto: "This is a thing to test multiple images on a single entry."
+            }
+        ],
+    ],
+
     init: function() {
         if($('#calendar').length > 0) {
             var currentMonth = new Date().getMonth();
@@ -148,6 +182,7 @@ calendar = {
     },
 
     loadMonth: function(monthNum) {
+        if(monthNum < 0 || monthNum >= 12) return false;
         var date = new Date(2016, monthNum),
             header = calendar.monthNames[date.getMonth()] + " " + date.getFullYear(),
             firstDay = date.getDay(),
@@ -155,6 +190,7 @@ calendar = {
             $calendar = $('#calendar');
         $calendar.data('currentMonth', monthNum);
         $calendar.find('.calendar-header .month-name').html(header);
+        $calendar.find('#dropdownMonth').html(calendar.monthNames[date.getMonth()] + ' <span class="caret"></span>');
         for(var i = 0, day = 1; i < 42; i++) {
              if( i >= 35 && day <= days && $('.calendar-row').length < 6) {
                  $('.calendar-days').append('<div class="calendar-row"><div class="calendar-grid"></div><div class="calendar-grid"></div><div class="calendar-grid"></div><div class="calendar-grid"></div><div class="calendar-grid"></div><div class="calendar-grid"></div><div class="calendar-grid"></div></div>');
@@ -166,9 +202,27 @@ calendar = {
              if(i < firstDay || day > days) {
                  $cell.html('&nbsp;');
              } else {
-                 $cell.html('<div class="day-number">' + day++ + '</div>');
+                 $cell.html('<div class="day-number" data-day="'+ day +'">' + day++ + '</div>');
              }
         }
+        if(monthNum == 0) {
+            $('.calendar-header .controls .glyphicon').eq(0).addClass('disabled')
+        } else if (monthNum == 11) {
+            $('.calendar-header .controls .glyphicon').eq(1).addClass('disabled')
+        } else {
+            $('.calendar-header .controls .glyphicon').removeClass('disabled')
+        }
+        calendar.populateMonth(monthNum);
+    },
+
+    populateMonth: function(monthNum) {
+        var $grids = $('div.calendar-grid');
+        $.each(calendar.data[monthNum], function(i, v) {
+            var $day = $grids.find('[data-day='+v.Día+']').first().parent();
+            if($day.find('.event').length == 0) $day.prepend("<div class='event'></div>");
+            $day.find('.event').append("<img src='" + calendar.categories[v.Categoría] + "'/>");
+            $day.find('.event img').last().data('key', monthNum + '-' + i);
+        });
     },
 
     setEvents: function() {
@@ -176,6 +230,34 @@ calendar = {
             var currentMonth = parseInt($('#calendar').data('currentMonth')),
                 tgtMonth = currentMonth + ($(this).index() == 0? -1:1);
             calendar.loadMonth(tgtMonth);
+        });
+
+        $('.calendar-header .controls .dropdown ul li').click(function(){
+            var tgtMonth = $(this).index();
+            calendar.loadMonth(tgtMonth);
+        });
+
+        $(document).on('click', '.event img', function() {
+            var key = $(this).data('key').split('-'),
+                data = calendar.data[key[0]][key[1]];
+            $('#eventModal .modal-title').html(data.Título);   
+            $('#imageWrapper > div').addClass('hidden');
+            if(data.Imagen_principal.length > 1) {
+                console.log('multiple images');
+                $("#modalSlider .carousel-inner").html('');
+                $.each(data.Imagen_principal, function(i, v) {
+                    var d = "<div class='item'><img src='" + v + "'/></div>"
+                    $("#modalSlider .carousel-inner").append(d);
+                });
+                $("#modalSlider .carousel-inner .item").first().addClass('active');
+                $("#modalSlider").carousel("pause").removeData();
+                $("#modalSlider").carousel(0);
+                $('#sliderWrapper').removeClass("hidden");
+            } else {                
+                $('#eventModal #mainImage').removeClass('hidden').html('<img class="modal-main-image" src="' + data.Imagen_principal[0] + '" />')
+            }
+            $('#eventModal .modal-text').html(data.Texto);        
+            $('#eventModal').modal('show');
         });
     }
 }
