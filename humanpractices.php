bdbb3c159033a5773ec25b122dc3e675e7e5ad07
Tigermisu <tigermisu@outlook.com>
2016-10-10 15:46:16 -0600

    hp and parts

--- humanpractices.php
+++ humanpractices.php
@@ -12,21 +12,151 @@
         </div>
     </div>
     <div class="section" style="background-color: #078794" data-anchor="humanpractices">
-        <img class="page-preview" src="http://igem.org/wiki/images/f/f2/T--Tec-Chihuahua--HP.jpg">
-    </div>        
+        <div class="sorted">
+            <div class="grow">
+                <p class="small">
+                    Science is not only about laboratory work or projects, but also context in social, geographical and global aspects. Synthetic Biology as a multidisciplinary science gathers different persons, scientists, and individuals. Therefore, it is essential to
+                    evaluate the circumstances at which every scientific project is developed, having in mind the responsibility that it carries. <br/> <br/> The implications of Synthetic Biology (hereinafter also referred to as SB)
+                    have repercussions from the Ethical, Economical and Legal perspectives. Our team analyzed said perspectives with a focus on the Latin-American dimension, for the diffusion of SB in the light of a responsible progress.
+                </p>
+            </div>
+            <div>
+                <img src="http://igem.org/wiki/images/f/f2/T--Tec-Chihuahua--HP.jpg" class="img-responsive">
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
     <div class="section" style="background-color: #F07033" data-anchor="ethics">
-        <img class="page-preview" src="http://igem.org/wiki/images/6/67/T--Tec-Chihuahua--Ethicsimg.jpg">
+        <div class="slide" data-anchor="1">
+            <div class="sorted">
+                <div>
+                    <img src="http://igem.org/wiki/images/6/67/T--Tec-Chihuahua--Ethicsimg.jpg" class="img-responsive">
+                </div>
+
+                <div class="grow">
+                    <p>
+                        The <strong>ethical</strong> approach focuses on the openness issue: the paradigms of exclusivity and attributions, its relation to the intellectual property (IP) system; the democratic and modular nature of SB; advancement and
+                        its implications concerning security.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide" data-anchor="2">
+            <div class="sorted">
+                <div class="grow">
+                    <p class="small">
+                        Openness. Individuals can contribute to a common source of knowledge that is accessible for everyone and can benefit the community Inclusivity. Discourse ethics defends a morality of equal respect and solidaristic responsibility<br/><br/>                        Exclusiveness and the IP conflict. The patent system is based in the principle that incentives are needed to have more creations and progress. SB involves biotechnology and software, opposite sciences concerning IP (the first one
+                        with patents, the second one with openness); therefore suggesting difficulties on the definition of Intellectual property for SB. <br/><br/> Progress. Progress is reached by advancements, but advancements not necessarily result
+                        in progress. Exclusiveness creates advancements, but do they create progress? Is the actual system (exclusiveness) democratic? <br/><br/> Democracy. Dialogue and participation are essential for SB, both to prevent harm and to ensure
+                        science is matter of every person (since it affects everyone equally).
+
+                    </p>
+                </div>
+                <div>
+                    <img src="http://igem.org/wiki/images/thumb/a/a2/T--Tec-Chihuahua--Meeting.jpg/769px-T--Tec-Chihuahua--Meeting.jpg.png" class="img-responsive">
+                </div>
+            </div>
+        </div>
     </div>
     <div class="section" style="background-color: #DA4E5B" data-anchor="economy">
-        <img class="page-preview" src="http://igem.org/wiki/images/e/ed/T--Tec-Chihuahua--Economy.png">
+        <div class="slide" data-anchor="1">
+            <div class="sorted">
+
+                <div class="grow">
+                    <p>
+                        From an <strong>Economic</strong> point of view, we discuss some of the effects that SB could cause, mainly in the context of developing economies and agriculture. SB could tend to increase the risk of displacement of individuals
+                        employed in the agricultural sector. Some of the Latin American economies could be at risk of being affected by the advancements of SB.
+                    </p>
+                </div>
+
+                <div>
+                    <img src="http://igem.org/wiki/images/e/ed/T--Tec-Chihuahua--Economy.png" class="img-responsive">
+                </div>
+            </div>
+        </div>
+        <div class="slide" data-anchor="2">
+            <div class="sorted">
+                <div>
+                    <img src="http://igem.org/wiki/images/thumb/c/c9/T--Tec-Chihuahua--science.jpg/355px-T--Tec-Chihuahua--science.jpg.png" class="img-responsive">
+                </div>
+                <div class="grow">
+                    <p>
+                        <i>Biotechnology Industry</i>. The advancements in scientific techniques, such as genetic manipulation has lead to advancements that have affected the existing economies. The arrival of Synthetic Biology. New technologies impact
+                        population, economic sectors, and industries. SB could have a great impact, both positive or negative, depending crucially on how the particular government of a country reacts to SB’s arrival.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="section" style="background-color: #87982E" data-anchor="legal">
-        <img class="page-preview" src="http://igem.org/wiki/images/1/1a/T--Tec-Chihuahua--Legal.jpg">
+        <div class="slide" data-anchor="1">
+            <div class="sorted">
+                <div>
+                    <img src="http://igem.org/wiki/images/1/1a/T--Tec-Chihuahua--Legal.jpg" class="img-responsive">
+                </div>
+
+                <div class="grow">
+                    <p>
+                        The <strong>Legal</strong> analysis focuses on the persistent need for an effective regulation of SB at international, regional and local level. The main legal aspects concerning SB regard the application of the IPR system, the
+                        effects of the possible application of contractual provisions as an alternative to IPR, and the issues concerning safety and security.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="slide" data-anchor="2">
+            <div class="sorted">
+                <div class="grow">
+                    <p class="small">
+                        Law for Synthetic Biology. The limits of IP could mine SB’s development. So, what kind of property regime would best suit SB: patents, copyrights or trademarks? It is also important to have in mind that an excess of property claims in science would make
+                        impossible for other people to use it.<br/><br/> Security risks and Bioterrorism. SB might provide “non-experts” with easier tools that could be used for negative purposes. Lowering the complexity of a science
+                        means also making it accessible to perpetrators with motivations distant from science. This is why awareness must be ensured, the strategy of diffusion must be enhanced. <br/><br/> Involuntary Effects of Synthetic Biology. Not
+                        every negative consequence from SB is intentional. Biocontainment is an effective alternative to prevent undesired repercussions of SB on the environment.
+
+                    </p>
+                </div>
+                <div>
+                    <img src="http://igem.org/wiki/images/thumb/0/00/T--Tec-Chihuahua--Laboratory.jpg/800px-T--Tec-Chihuahua--Laboratory.jpg.png" class="img-responsive">
+                </div>
+            </div>
+        </div>
     </div>
     <div class="section" style="background-color: #078794" data-anchor="interview">
-        <img class="page-preview" src="http://igem.org/wiki/images/8/87/T--Tec-Chihuahua--Interview.jpg">
+        <div class="slide" data-anchor="1">
+            <div class="sorted">
+
+                <div class="grow">
+                    <p>
+                        From an <strong>Economic</strong> point of view, we discuss some of the effects that SB could cause, mainly in the context of developing economies and agriculture. SB could tend to increase the risk of displacement of individuals
+                        employed in the agricultural sector. Some of the Latin American economies could be at risk of being affected by the advancements of SB.
+                    </p>
+                </div>
+
+                <div>
+                    <img src="http://igem.org/wiki/images/8/87/T--Tec-Chihuahua--Interview.jpg" class="img-responsive">
+                </div>
+            </div>
+        </div>
+        <div class="slide" data-anchor="2">
+            <div class="sorted">
+                <div>
+                    <img src="http://igem.org/wiki/images/thumb/c/c9/T--Tec-Chihuahua--science.jpg/355px-T--Tec-Chihuahua--science.jpg.png" class="img-responsive">
+                </div>
+                <div class="grow">
+                    <p>
+                        Synthetic Biology: A look through the Lens of Latin America
+
+
+                    </p>
+                    <p class="small">
+                        The second part of diffusion in our project is a document where you can see on detail the analysis made by our team in the Ethical, Economical and Legal aspects of Synthetic Biology in Latin America. We analyzed the above mentioned problems, in order
+                        to propose possible solutions that aim to be incorporated at regulamentary level. Our guidelines can be employed by companies, associations and governments to develop provisions regulating quality, management of bioparts, safety,
+                        as well as reconcile the main economic, ethical and legal issues surrounding SB. <br/> <br/> You can see our Guidelines in <a href="#languagePreview" data-toggle="modal"> this preview.</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="section fp-auto-height" style="background-color: #333" data-anchor="footer">     
+    <div class="section fp-auto-height" style="background-color: #333" data-anchor="footer">
         <?php include('footer-nav.html') ?>
     </div>
 </div>
@@ -42,6 +172,34 @@
 <div class="control-arrows">
     <span class="glyphicon glyphicon-chevron-up nope" id="upArrow"></span>
     <span class="glyphicon glyphicon-chevron-down" id="downArrow"></span>
-</div>    
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="languagePreview">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Synthetic Biology: A look through the Lens of Latin America</h4>
+            </div>
+            <div class="modal-body">
+                <p>In order to reach a broader diffusion of SB, we developed our Guidelines in 3 languages! Choose a language to continue</p>
+                <div class="row margin">
+                    <div class="col-xs-4" style="text-align: center;">
+                        <a href="">English</a>
+                    </div>
+                    <div class="col-xs-4" style="text-align: center;">
+                        <a href="">Spanish</a>
+                    </div>
+                    <div class="col-xs-4" style="text-align: center;">
+                        <a href="">Portuguese</a>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include('minfooter.html'); ?>
--- projectparts.php
+++ projectparts.php
@@ -1,17 +1,184 @@
 <?php include('header.html'); ?>
 <span id="meta" data-this-page="project"></span>
-<div class="nav-backdrop"></div>
-<div class="mini-cover" style="background-image: url()">
-    <div class="overlay"></div>
-    <div class="gradient-overlay"></div>
-    <div class="content">
-        <h1>Parts</h1>
+
+<div id="fullpage">
+    <div class="section fp-auto-height" data-anchor="home">
+        <div class="mini-cover" style="background-image: url()">
+            <div class="overlay"></div>
+            <div class="gradient-overlay"></div>
+            <div class="content">
+                <h1>Parts</h1>
+            </div>
+        </div>
+    </div>
+    <div class="section" style="background-color: #078794" data-anchor="used">
+        <div class="sorted">
+            <div class="grow">
+                <p>
+                    <strong>Basic parts</strong> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K541506">BBa_K541506:  Reflectin1A from cephalopod (2)</a><br/ >
+                    - <a data-toggle="modal" href="#BBa_B0034">BBa_B0034: RBS (4)</a> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K538000">BBa_K538000: Cpn10 (5)</a> <br/ >  <br/ >
+
+	                <strong>Construction intermediates</strong> <br/ >
+		            - <a data-toggle="modal" href="#BBa_K525998">BBa_K525998: T7 promoter + RBS (1)</a> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K538101">BBa_K538101: RBS + Cpn60 (6)</a> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K1364007">BBa_K1364007: RBS + Antifungal GAFP-1 + Double terminator (3)</a> <br/ >
+                </p>
+            </div>
+            <div>
+                <img src="http://igem.org/wiki/images/9/9f/T--Tec-Chihuahua--Partsimg.jpg" class="img-responsive">
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>    
+    <div class="section" style="background-color: #87982E" data-anchor="submitted">
+        <div class="sorted">            
+            <div>
+                <img src="http://igem.org/wiki/images/9/9f/T--Tec-Chihuahua--Partsimg.jpg" class="img-responsive">
+            </div>
+            <div class="grow">
+                <p>
+                    <strong>Basic parts</strong> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K541506">BBa_K541506:  Reflectin1A from cephalopod (2)</a><br/ >
+                    - <a data-toggle="modal" href="#BBa_B0034">BBa_B0034: RBS (4)</a> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K538000">BBa_K538000: Cpn10 (5)</a> <br/ >  <br/ >
+
+	                <strong>Construction intermediates</strong> <br/ >
+		            - <a data-toggle="modal" href="#BBa_K525998">BBa_K525998: T7 promoter + RBS (1)</a> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K538101">BBa_K538101: RBS + Cpn60 (6)</a> <br/ >
+                    - <a data-toggle="modal" href="#BBa_K1364007">BBa_K1364007: RBS + Antifungal GAFP-1 + Double terminator (3)</a> <br/ >
+                </p>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>    
+    <div class="section fp-auto-height" style="background-color: #333" data-anchor="footer">
+        <?php include('footer-nav.html') ?>
     </div>
 </div>
 
+<ul id="slideMenu" class="slide-menu">
+    <li data-menuanchor="used"><a href="#used">New Parts</a></li>
+    <li data-menuanchor="submitted"><a href="#submitted">Submitted Parts</a></li>
+</ul>
 
+<div class="control-arrows">
+    <span class="glyphicon glyphicon-chevron-up nope" id="upArrow"></span>
+    <span class="glyphicon glyphicon-chevron-down" id="downArrow"></span>
+</div>
 
-<img src="http://igem.org/wiki/images/9/9f/T--Tec-Chihuahua--Partsimg.jpg" class="page-preview">
+<div class="modal fade" tabindex="-1" role="dialog" id="BBa_K541506">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">BBa_K541506:  Reflectin 1A from cephalopod</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    This part is the Coding Sequence of a self-assembling protein that has the capacity to reflect sunlight, depending on the thickness of the protein layer, it is expressed on the outer membrane of gram negative bacteria. This protein will be used to measure protein expression.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="modal fade" tabindex="-1" role="dialog" id="BBa_B0034">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">BBa_B0034: RBS</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    This part is the Ribosome Binding Site based on the Elowitz (1999) repressilator. It is a strong RBS, defining the relative strength of 1 for all other RBSs in the iGEM registry.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
 
-<?php include('footer.html'); ?>
+<div class="modal fade" tabindex="-1" role="dialog" id="BBa_K538000">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">BBa_K538000: Cpn10</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    This part encodes the cochaperonin 10, which is part of the Cpn60/10 chaperone system of Oleispira antarctica. When coexpressed with chaperonin 60 forms a heteromultimer that can refold enzymes at very low temperatures, thus safeguarding their functionality. This has been shown to enable E. coli to grow even at freezing point. This is a part from Amsterdam’s 2011 iGEM Cryobricks.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="BBa_K525998">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">BBa_K525998: T7 promoter + RBS</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                   This part contains the phage T7 promoter along with the BBa_B0034 Ribosome Binding Site. This specific promoter was chosen for overexpression and capacity of the Myxobateria RNA polymerase to read phage promoters, such as T7.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="BBa_K541506">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">BBa_K541506:  Reflectin 1A from cephalopod</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    This is a translational unit that consists of the BBa_B0034 RBS and the CDS of the protein chaperonin 60, which is part of the Cpn60/10 chaperone system of Oleispira antarctica. When coexpressed with cochaperonin 10 forms a heteromultimer that can refold enzymes at very low temperatures, thus safeguarding their functionality. This has been shown to enable E. coli to grow even at freezing point. This is a part from Amsterdam’s 2011 iGEM Cryobricks.    
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" tabindex="-1" role="dialog" id="BBa_K1364007">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">BBa_K1364007: RBS + Antifungal GAFP-1 + Double terminator</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    This part encodes a translational unit that contains the BBa_K780002 RBS, which is a strong promoter on Bacillus subtilis, but works with a strength of 10.13% relative to the BBa_B0032 RBS on E. coli; and the CDS of Gastrodia anti-fungal protein, which is a mannose and chitin binding lectin originating from the Asiatic orchid Gastrodia elata that is able to inhibit the growth of multiples species of plant pathogenic fungi. This part also includes two terminators, B0010 and B0012.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('minfooter.html'); ?>
